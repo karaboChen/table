55959bfba7fa9a734302c4b8b70bd595d4343355
karaboChen <nn0907137897@gmail.com>
2024-05-30 01:02:28 +0800

    996

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,7 @@
 <script setup>
-import { ref, onMounted, nextTick } from "vue";
-import * as signalR from "@microsoft/signalr";
-let connection = null;
-const tb = ref(null);
-const highlightedId = ref(null);
-onMounted(async () => {
-  //禁止協商方式
-  // let  option = {
-  //   skipNegotiation: true, transport:signalR.HttpTransportType.WebSockets
-  // }
-  connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:5120/myhub") //後面可加參數 withUrl("http://localhost:5120/myhub", option)
-    .withAutomaticReconnect()
-    .build();
-  await connection.start();
-  //這邊要監聽 後端給的事件名稱
-  connection.on("接收公共訊息", (data) => {
-    console.log(data);
-    tableData.value = data;
-  });
-
-  connection.on("aaa", (x, y, data) => {
-    // tableData.value[x][y].id=data
-    tableData.value[x].id = data;
-    highlightedId.value = "99";
-    setTimeout(() => {
-      highlightedId.value = null;
-    }, 2000);
-  });
-});
-
-async function ww() {
-  await connection.invoke("Sendmessage");
-}
-
-async function ww1() {
-  await connection.invoke("Sendmessage1", 0, 1, "99");
-}
-
-const tableData = ref([]);
+import Virtual from "@/components/Virtual.vue";
 </script>
 
 <template>
-  <el-table ref="tb" :data="tableData" style="width: 100%">
-    <el-table-column prop="id" label="Date" width="180">
-      <template #default="scope">
-        <div :class="{ highlight: scope.row.id === highlightedId }">
-          {{ scope.row.id }}
-        </div>
-      </template>
-    </el-table-column>
-    <el-table-column prop="name" label="Name" width="180" />
-  </el-table>
-  <button @click="ww">++</button>
-  <button @click="ww1">--</button>
+  <Virtual />
 </template>
-
-<style>
-.highlight {
-  background-color: yellow;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import './assets/reset.scss'
-import { createApp } from 'vue'
-import App from './App.vue'
-import * as echarts from 'echarts';
+import "./assets/reset.scss";
+import { createApp } from "vue";
+import i18n from "./compasble/index";
+import VXETable from "vxe-table";
+import "vxe-table/lib/style.css";
+import App from "./App.vue";
+import * as echarts from "echarts";
 const app = createApp(App);
 
+VXETable.setConfig({
+  // 对组件内置的提示语进行国际化翻译
+  i18n: (key, args) => i18n.global.t(key, args),
+});
 
+app.use(VXETable);
 app.config.globalProperties.$echarts = echarts;
-app.mount('#app')
+app.mount("#app");
